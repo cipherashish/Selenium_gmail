e00cd4cc9a4e58a8548b291cda2075354c6f2558
cipherashish <ashish.090singh@gmail.com>
2019-09-03 18:35:18 +0530

    commit

---
+++ src/com/selenium/test/Demo.java
@@ -0,0 +1,36 @@
+package com.selenium.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+	System.setProperty("webdriver.chrome.driver", "E:\\avan\\chromedriver_win32\\chromedriver.exe");
+  WebDriver driver =new ChromeDriver();
+  driver.get("http://www.gmail.com/");
+  driver.manage().window().maximize();
+  driver.findElement(By.id("identifierId")).sendKeys("@gmail.com");
+  Thread.sleep(2000);
+  driver.findElement(By.className("CwaK9")).click();
+  Thread.sleep(2000);
+String at =driver.getTitle();
+String et ="no";//expected title
+driver.close();
+
+
+//establish test condition
+
+if(at.equalsIgnoreCase(et))
+{
+	System.out.println("test successful");
+}
+
+else {
+	System.out.println("test failure");
+}
+	}
+
+}
