1178ee1d33670f3b770c1a8d987fcc3965e72449
cipherashish <50736885+cipherashish@users.noreply.github.com>
2019-10-16 17:23:27 +0530

    Update Demo.java

--- src/com/selenium/test/Demo.java
+++ src/com/selenium/test/Demo.java
@@ -6,10 +6,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Demo {
 
+	
 	public static void main(String[] args) throws InterruptedException {
-		
-	System.setProperty("webdriver.chrome.driver", "/home/e823409/Driver-master/chromedriver.exe");
-  WebDriver driver =new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//Driver/chromedriver.exe"); // <– Change this path
+	ChromeOptions options = new ChromeOptions();
+	options.addArguments("headless");
+	options.addArguments("no-sandbox");
+	options.addArguments("disable-dev-shm-usage");
+	options.addArguments("window-size=1200x600");
+	WebDriver driver = new ChromeDriver(options);
   driver.get("http://www.gmail.com/");
   driver.manage().window().maximize();
   driver.findElement(By.id("identifierId")).sendKeys("@gmail.com");
